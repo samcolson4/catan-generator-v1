c8a4ec23cbbd38c14ba70d608de6edfca547ee2c
samcolson4 <samcolson4@gmail.com>
2021-01-26 22:26:17 +0000

    Allocate changes row one value

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,3 +1,17 @@
 class Generator
-  
+
+  attr_reader :row_one
+
+  def initialize
+    @row_one = []
+    @row_two = []
+    @row_three = []
+    @row_four = []
+    @row_five = []
+  end
+
+  def allocate 
+    @row_one = [1, 2, 3]
+  end
+
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,4 +2,11 @@ require 'generator'
 
 describe Generator do
   
+  describe '#allocate' do
+    it 'adds three hexes to row one' do
+      subject.allocate
+      expect(subject.row_one.length).to eq 3
+    end
+  end
+
 end
