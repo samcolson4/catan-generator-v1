31fb80850ad979a0fad59ebbdc9ad82a2ffcb697
samcolson4 <samcolson4@gmail.com>
2021-01-27 09:36:10 +0000

    Add edge case tests

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -6,6 +6,21 @@ describe Generator do
       subject.allocate_hexes
       expect(subject.board_hexes.length).to eq subject.hexes.length
     end
+
+    it 'contains wheat' do
+      subject.allocate_hexes
+      expect(subject.board_hexes).to include("wheat")
+    end
+
+    it 'contains ore' do
+      subject.allocate_hexes
+      expect(subject.board_hexes).to include("wheat")
+    end
+
+    it 'does not contain a hex that does not exist' do
+      subject.allocate_hexes
+      expect(subject.board_hexes).to_not include("sand")
+    end
   end
 
   describe '#allocate_numbers' do
