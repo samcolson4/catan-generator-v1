4faa903d1c6f58b118959eb0f8f857ffb48c772c
samcolson4 <samcolson4@gmail.com>
2021-01-27 07:48:33 +0000

    Add randomise_and_pick method

--- lib/generator.rb
+++ lib/generator.rb
@@ -2,7 +2,7 @@ class Generator
 
   HEXES = ["wheat", "wheat", "wheat", "wheat", "ore", "ore", "ore", "brick", "brick", "brick", "sheep", "sheep", "sheep", "sheep", "lumber", "lumber", "lumber", "lumber", "lumber", "desert"]
 
-  attr_reader :row_one, :row_two, :row_three, :row_four, :row_five
+  attr_reader :row_one, :row_two, :row_three, :row_four, :row_five, :hexes
 
   def initialize(hexes = HEXES)
     @row_one = []
@@ -15,40 +15,15 @@ class Generator
   end
 
   def allocate
-    @board.each { |row|
-      while !rows_full?
-        row << "hex"
-      end
-      # row << "hex"
-      # row << "hex"
-      
-    }
-    # @row_one = [1, 2, 3]
-    # @row_two = [1, 2, 3, 4]
-    # @row_three = [1, 2, 3, 4, 5]
-    # @row_four = [1, 2, 3, 4]
-    # @row_five = [1, 2, 3]
+  end
+
+  def randomise_and_pick
+    index = rand(0..@hexes.length)
+    hex = @hexes[index]
+    @hexes.delete_at(index)
+    hex
   end
 
   private
-
-  def rows_full?
-    row_one_full? ||  row_two_full?
-  end
-
-  def row_one_full?
-    @row_one.length == 3
-  end
-
-  def row_two_full?
-    @row_two.length == 4
-  end
-
-  def row_not_full?
-    @row_two.length < 4
-    @row_three.length < 5
-    @row_four.length < 4
-    @row_five.length < 3
-  end
 
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,31 +2,37 @@ require 'generator'
 
 describe Generator do
   
-  describe '#allocate' do
-    it 'adds three hexes to row_one' do
-      subject.allocate
-      p subject.row_one
-      expect(subject.row_one.length).to eq 3
-    end
+  # describe '#allocate' do
+  #   it 'adds three hexes to row_one' do
+  #     subject.allocate
+  #     p subject.row_one
+  #     expect(subject.row_one.length).to eq 3
+  #   end
 
-    it 'adds four hexes to row_two' do
-      subject.allocate
-      expect(subject.row_two.length).to eq 4
-    end
+  #   it 'adds four hexes to row_two' do
+  #     subject.allocate
+  #     expect(subject.row_two.length).to eq 4
+  #   end
 
-    it 'adds five hexes to row_three' do
-      subject.allocate
-      expect(subject.row_three.length).to eq 5
-    end
+  #   it 'adds five hexes to row_three' do
+  #     subject.allocate
+  #     expect(subject.row_three.length).to eq 5
+  #   end
 
-    it 'adds four hexes to row_four' do
-      subject.allocate
-      expect(subject.row_four.length).to eq 4
-    end
+  #   it 'adds four hexes to row_four' do
+  #     subject.allocate
+  #     expect(subject.row_four.length).to eq 4
+  #   end
 
-    it 'adds three hexes to row_five' do
-      subject.allocate
-      expect(subject.row_five.length).to eq 3
+  #   it 'adds three hexes to row_five' do
+  #     subject.allocate
+  #     expect(subject.row_five.length).to eq 3
+  #   end
+  # end
+
+  describe '#randomise_and_pick' do
+    it 'removes one hex from the total list' do
+      expect { subject.randomise_and_pick }.to change { subject.hexes.length }.by(-1)
     end
   end
 
